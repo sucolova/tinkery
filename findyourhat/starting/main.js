3592691a116c85720257df7b164e03b3394c3c23
sucolova <lukas.melcher@gmail.com>
2022-06-03 16:07:29 +0200

    Automated sync:

--- findyourhat/starting/main.js
+++ findyourhat/starting/main.js
@@ -4,3 +4,21 @@ const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+
+class Field {
+    constructor(fieldArray){
+        this._fieldArray = fieldArray;
+    }
+    get fieldArray(){
+        return this._fieldArray;
+    }
+    sayHello(){
+        const name = prompt("give me your name!!! i am not kidding around here");
+        console.log(`hello ${name}, nice to meet you!`);
+    }
+}
+
+let testField = new Field();
+
+testField.sayHello();
+
